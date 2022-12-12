2c11a60516e11eac7902b5cc612ccacd5d490e7f
tashenoff <kail.dead.kd@gmail.com>
2022-12-11 21:13:41 -0500

    .

--- header.php
+++ header.php
@@ -94,7 +94,7 @@
 														d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 												</svg>
 
-												<a class="" href="soft.html">НПА</a>
+												<a class="" href="/npa">НПА</a>
 											</li>
 											<li>
 												<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
